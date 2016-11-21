0fa125f2df4c93ec0074e18f7c6de0d570007386
Matt Lewis <matt@mplewis.com>
2016-11-20 17:22:58 -0700

    app: refactor config hardcoded; show swinginess/results

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ import 'jquery.easing'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './app.css'
 
+import config from './config'
 import appTemplate from './app.pug'
 import electionData from './data/election_data.json'
 
 const data = {
-  states: electionData
+  states: process(electionData)
 }
 
 document.getElementById('app').innerHTML = appTemplate(data)
@@ -17,11 +18,28 @@ document.getElementById('app').innerHTML = appTemplate(data)
 const statePicker = $('#state-picker')
 const states = $('#states')
 
-function onStateSelected () {
+statePicker.change(() => {
   states.show()
   const slug = statePicker.val()
   const target = '#state-' + slug
-  $(window).scrollTo(target, 1500, {easing: 'easeOutQuart'})
+  $(window).scrollTo(target,
+    config.scroll.duration,
+    {easing: config.scroll.easing})
+})
+
+function swinginess (state) {
+  return state.history
+    .map((data) => data.dem_win_margin)
+    .filter((margin) => Math.abs(margin) <= config.swinginessMargin)
+    .length
 }
 
-statePicker.change(onStateSelected)
+function process (electionData) {
+  electionData.forEach(state => {
+    state.swinginess = swinginess(state)
+    state.history.forEach(year => {
+      year.close = Math.abs(year.dem_win_margin) <= config.swinginessMargin
+    })
+  })
+  return electionData
+}
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  swinginessMargin: 3,
+  scroll: {
+    duration: 1500,
+    easing: 'easeOutQuart'
+  }
+}
+
+export default config
