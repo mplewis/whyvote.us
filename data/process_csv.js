61a6e125b2c5e1b1918c2fd10d65a98d9c757954
Matt Lewis <matt@mplewis.com>
2017-05-20 12:30:22 -0600

    mostly working table processing

--- data/process_csv.js
+++ data/process_csv.js
@@ -25,12 +25,34 @@ let table = parse(raw)
 let cols = transpose(table)
 // find the index of the first column with all empty cells
 const firstEmptyCol = cols.map(col => col.map(Boolean).reduce((a, b) => a || b)).findIndex(i => !i)
-
 // drop all cols between state names col and including first empty col
 cols = [cols[0]].concat(cols.slice(firstEmptyCol + 1))
+table = transpose(cols)
+
+// extend all Year columns into empty cells to the right
+const header = table[0]
+for (let i = 1; i < header.length; i++) {
+  if (!header[i]) header[i] = header[i - 1]
+}
+cols = transpose(table)
+
 // drop all cols at and after the years we don't care about
 const dropYearCol = cols.findIndex(c => c[0] === dropYearsAfter)
 cols = cols.slice(0, dropYearCol)
+// drop all Total columns
+const colsToDrop = cols
+  .map(col => col[1] === 'Total')
+  .map((val, index) => val && index)
+  .filter(Boolean)
+colsToDrop.reverse()
+colsToDrop.forEach(colNum => { cols = cols.slice(0, colNum).concat(cols.slice(colNum + 1)) })
 
-// transpose back into a row-based table
 table = transpose(cols)
+
+// drop rows with regions - states and regions are separated by a blank row
+const dropRegionsRow = cols[0].findIndex(cell => cell === '')
+table = table.slice(0, dropRegionsRow)
+
+// drop nationwide row
+const dropNationwideRow = cols[0].findIndex(cell => cell === 'Nationwide')
+table = table.slice(0, dropNationwideRow).concat(table.slice(dropNationwideRow + 1))
